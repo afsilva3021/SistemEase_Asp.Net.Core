58d10b28b52e8c7fb7f3f97df1d4cccfa2164039
Alex felix <afsilva3021@gmail.com>
2024-03-06 08:04:20 -0300

    Versao alpha do sistema em desenvolvimento

---
+++ SistemEase/Controllers/Dashboard.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemEase.Models;
+using System.Diagnostics;
+
+
+namespace SistemEase.Controllers
+{
+    public class Dashboard : Controller
+    {
+        private readonly ILogger _logger;
+
+
+
+    }
+}
